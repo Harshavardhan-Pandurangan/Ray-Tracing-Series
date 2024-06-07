d316c4487f99cdb78435910947dc78b0531c3d7c
Harshavardhan-Pandurangan <rockingharsha71@gmail.com>
2024-06-08 01:41:03 +0530

    completed lights and setting up the cornell box

--- book-2/perlin.h
+++ book-2/perlin.h
@@ -8,10 +8,10 @@ class perlin
 public:
     perlin()
     {
-        randfloat = new double[point_count];
+        randvec = new vec3[point_count];
         for (int i = 0; i < point_count; i++)
         {
-            randfloat[i] = random_double();
+            randvec[i] = unit_vector(vec3::random(-1, 1));
         }
 
         perm_x = perlin_generate_perm();
@@ -21,7 +21,7 @@ public:
 
     ~perlin()
     {
-        delete[] randfloat;
+        delete[] randvec;
         delete[] perm_x;
         delete[] perm_y;
         delete[] perm_z;
@@ -36,21 +36,37 @@ public:
         auto i = int(floor(p.x()));
         auto j = int(floor(p.y()));
         auto k = int(floor(p.z()));
-        double c[2][2][2];
+        vec3 c[2][2][2];
 
         for (int di = 0; di < 2; di++)
             for (int dj = 0; dj < 2; dj++)
                 for (int dk = 0; dk < 2; dk++)
-                    c[di][dj][dk] = randfloat[perm_x[(i + di) & 255] ^
-                                              perm_y[(j + dj) & 255] ^
-                                              perm_z[(k + dk) & 255]];
+                    c[di][dj][dk] = randvec[perm_x[(i + di) & 255] ^
+                                            perm_y[(j + dj) & 255] ^
+                                            perm_z[(k + dk) & 255]];
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
+    }
+
+    double turb(const point3 &p, int depth) const
+    {
+        auto accum = 0.0;
+        auto temp_p = p;
+        auto weight = 1.0;
+
+        for (int i = 0; i < depth; i++)
+        {
+            accum += weight * noise(temp_p);
+            weight *= 0.5;
+            temp_p *= 2;
+        }
+
+        return fabs(accum);
     }
 
 private:
     static const int point_count = 256;
-    double *randfloat;
+    vec3 *randvec;
     int *perm_x;
     int *perm_y;
     int *perm_z;
@@ -78,15 +94,22 @@ private:
         }
     }
 
-    static double trilinear_interp(double c[2][2][2], double u, double v, double w)
+    static double perlin_interp(const vec3 c[2][2][2], double u, double v, double w)
     {
+        auto uu = u * u * (3 - 2 * u);
+        auto vv = v * v * (3 - 2 * v);
+        auto ww = w * w * (3 - 2 * w);
         auto accum = 0.0;
+
         for (int i = 0; i < 2; i++)
             for (int j = 0; j < 2; j++)
                 for (int k = 0; k < 2; k++)
-                    accum += (i * u + (1 - i) * (1 - u)) *
-                             (j * v + (1 - j) * (1 - v)) *
-                             (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+                {
+                    vec3 weight_v(u - i, v - j, w - k);
+                    accum += (i * uu + (1 - i) * (1 - u)) *
+                             (j * vv + (1 - j) * (1 - v)) *
+                             (k * ww + (1 - k) * (1 - w)) * dot(c[i][j][k], weight_v);
+                }
 
         return accum;
     }
